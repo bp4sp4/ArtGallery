ca0c87d26b6abbb3e016914c822db39d1a29c751
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-01-17 13:41:14 +0900

    new CSSGrid with NextJs Project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  basePath: isProd ? "/ArtGallery" : "",
+  assetPrefix: isProd ? "/ArtGallery/" : "",
 };
-
-export default nextConfig;
